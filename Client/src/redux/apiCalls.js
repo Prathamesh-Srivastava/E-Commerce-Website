08004bb2da037acfa5f02c320bd42838fec93bc2
Prathamesh-Srivastava <prathameshsrivastava04@gmail.com>
2023-06-09 01:03:18 +0530

    Change of axios link inapiCalls

--- Client/src/redux/apiCalls.js
+++ Client/src/redux/apiCalls.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const login = async (dispatch,user) => {
     dispatch(loginStart());
     try {
-        const res = await axios.post("http://localhost:80/api/auth/login",user);
+        const res = await axios.post("https://ecommerce-server-k26h.onrender.com/api/auth/login",user);
         dispatch(loginSuccess(res.data));
     } catch (error) {
         dispatch(loginFailure());
@@ -15,7 +15,7 @@ export const login = async (dispatch,user) => {
 export const register = async (dispatch,user) =>{
     dispatch(registerStart());
     try {
-        const res = await axios.post("http://localhost:80/api/auth/register",user);
+        const res = await axios.post("https://ecommerce-server-k26h.onrender.com/api/auth/register",user);
         dispatch(registerSuccess(res.data));
     } catch (error) {
         dispatch(registerFailure());   
